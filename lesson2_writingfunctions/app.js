f3186105a09879cd3a53e6f0ea69cd29f010b69c
Sougato <sougato.chakraborty@sap.com>
2021-09-02 21:11:20 +0530

    First day

---
+++ lesson2_writingfunctions/app.js
@@ -0,0 +1,7 @@
+function sum( a , b){
+    return a + b;
+}
+
+//var let const
+const total = sum(20,30);//in js functions are 1st class citizens, functions can be assigned toa var and can be passed as arguments
+console.log("Total: ", total);
---
+++ lesson5_objectdestructuring/app.js
@@ -0,0 +1,7 @@
+//console.log("Process: ", process);
+// //var let const
+const {sum} = require("./helpers");
+const total = sum(20,30);//in js functions are 1st class citizens, functions can be assigned toa var and can be passed as arguments
+ console.log("Total: ", total);
+
+// console.log("Process: ", process);
